a14951b0a96a4c122d207e8615892a9ae69e9943
H. Sabah <sabahhasbi00@gmail.com>
2023-11-02 18:09:41 +0000

    Code fix: added missing dev, task 4 seems working

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,15 +1,41 @@
-$(document).ready(function() {
-	$.get("http://localhost:5001/api/v1/status/", function(data, textStatus) {
+$(document).ready(function () {
+	$.ajax({
+		type: 'POST',
+		url: 'http://localhost:5001/api/v1/places_search',
+		data: '{}',
+		dataType: 'json',
+		contentType: 'application/json',
+		success: function (data) {
+			for (let i = 0; i < data.length; i++) {
+				let place = data[i];
+				$('.places').append(`
+				<article>
+					<div class="title_box">
+						<h2>${place.name}</h2>
+						<div class="price_by_night">$${place.price_by_night}</div>
+					</div>
+					<div class="information">
+						<div class="max_guest">${place.max_guest} Guest(s)</div>
+							<div class="number_rooms">${place.number_rooms} Bedroom(s)</div>
+							<div class="number_bathrooms">${place.number_rooms} Bathroom(s)</div>
+					</div>
+					<div class="description">${place.description}</div>
+				</article>
+			  `);
+			}
+		}
+	})
+	$.get("http://localhost:5001/api/v1/status/", function (data, textStatus) {
 		if (data.status === "OK")
 			$("#api_status").addClass("available")
 		else
 			$("#api_status").removeClass("available")
 	})
 
-  let amenity_list = [];
-  $("INPUT").change(function() {
-    if ($(this).is(":checked"))
-      amenity_list.push($(this).attr("data-name"))
+	let amenity_list = [];
+	$("INPUT").change(function () {
+		if ($(this).is(":checked"))
+			amenity_list.push($(this).attr("data-name"))
 		else {
 			let index = amenity_list.indexOf($(this).attr("data-name"));
 			amenity_list.splice(index, 1)
@@ -19,5 +45,5 @@ $(document).ready(function() {
 			$(".amenities h4").text(formatted)
 		else
 			$(".amenities h4").html("&nbsp;")
-  })
+	})
 });
